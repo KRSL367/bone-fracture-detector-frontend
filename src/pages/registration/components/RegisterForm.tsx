0cb186151f36e575a640853372e910af54c46560
KRSL367 <krishalpudasaini12345@gmail.com>
2024-07-28 16:04:53 +0545

    Input component is used in registerform

--- src/pages/registration/components/RegisterForm.tsx
+++ src/pages/registration/components/RegisterForm.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import Label from "./Label";
+import Input from "./Input";
 
-// Define the interface for the props
 interface RegisterFormProps {
   onSubmit: (formData: {
     username: string;
@@ -9,9 +10,10 @@ interface RegisterFormProps {
   }) => void;
 }
 
-// Component definition with props
 const RegisterForm: React.FC<RegisterFormProps> = ({ onSubmit }) => {
   const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: "",
     username: "",
     email: "",
     password: "",
@@ -47,127 +49,91 @@ const RegisterForm: React.FC<RegisterFormProps> = ({ onSubmit }) => {
   return (
     <form onSubmit={handleSubmit} className="space-y-4">
       <div>
-        <label
-          htmlFor="firstname"
-          className="block text-sm font-medium text-gray-700"
-        >
-          First name
-        </label>
-        <input
-          type="text"
+        <Label htmlFor="firstname">First name</Label>
+        <Input
           id="firstname"
-          value={formData.username}
+          type="text"
+          value={formData.firstname}
           onChange={handleChange}
           placeholder="Jesse"
-          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           required
         />
       </div>
 
       <div>
-        <label
-          htmlFor="lastname"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Last name
-        </label>
-        <input
-          type="text"
+        <Label htmlFor="lastname">Last name</Label>
+        <Input
           id="lastname"
-          value={formData.username}
+          type="text"
+          value={formData.lastname}
           onChange={handleChange}
           placeholder="Pinkman"
-          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           required
         />
       </div>
 
       <div>
-        <label
-          htmlFor="username"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Username
-        </label>
-        <input
-          type="text"
+        <Label htmlFor="username">Username</Label>
+        <Input
           id="username"
+          type="text"
           value={formData.username}
           onChange={handleChange}
           placeholder="Jessepinkman123"
-          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           required
         />
       </div>
 
       <div>
-        <label
-          htmlFor="email"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Email
-        </label>
-        <input
-          type="email"
+        <Label htmlFor="email">Email</Label>
+        <Input
           id="email"
+          type="email"
           value={formData.email}
           onChange={handleChange}
           placeholder="jesse.pinkman@example.com"
-          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           required
         />
       </div>
 
       <div>
-        <label
-          htmlFor="password"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Password
-        </label>
-        <input
-          type={showPassword ? "text" : "password"}
+        <Label htmlFor="password">Password</Label>
+        <Input
           id="password"
+          type={showPassword ? "text" : "password"}
           value={formData.password}
           onChange={handleChange}
           placeholder="Enter your password"
-          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           required
         />
       </div>
 
       <div>
-        <label
-          htmlFor="confirmPassword"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Confirm Password
-        </label>
-        <input
-          type={showPassword ? "text" : "password"}
+        <Label htmlFor="confirmPassword">Confirm Password</Label>
+        <Input
           id="confirmPassword"
+          type={showPassword ? "text" : "password"}
           value={formData.confirmPassword}
           onChange={handleChange}
           placeholder="Confirm your password"
-          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           required
         />
       </div>
 
       <div className="flex items-center">
-        <input
-          type="checkbox"
+        <Input
           id="showPassword"
-          checked={showPassword}
+          type="checkbox"
+          value={showPassword.toString()}
           onChange={toggleShowPassword}
           className="h-4 w-4 text-indigo-600 border-gray-300 rounded"
         />
-        <label
+        <Label
           htmlFor="showPassword"
           className="ml-2 block text-sm text-gray-900"
         >
           Show Password
-        </label>
+        </Label>
       </div>
 
       <button
