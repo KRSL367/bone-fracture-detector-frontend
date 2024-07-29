495337df88e4ee5dbf0248a8d6c52e178f500364
KRSL367 <krishalpudasaini12345@gmail.com>
2024-07-29 14:08:31 +0545

    Completed Login hooks and sending request

--- src/pages/login/components/LoginForm.tsx
+++ src/pages/login/components/LoginForm.tsx
@@ -1,45 +1,29 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { registerSchema, RegisterSchema } from "../validation/registerSchema";
-import Label from "./Label";
-import CustomInput from "./CustomInput";
-import { useRegisterUser } from "../hooks/UseRegisterData";
+import { LoginSchema, loginSchema } from "../validations/loginSchema";
+import Label from "../../../components/Label";
+import CustomInput from "../../../components/CustomInput";
+import { useLoginUser } from "../hooks/useLoginUser";
 
 const RegisterForm: React.FC = () => {
   const [showPassword, setShowPassword] = useState(false);
-  const { register, handleSubmit, formState: { errors } } = useForm<RegisterSchema>({
-    resolver: zodResolver(registerSchema),
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
   });
 
-  const { registerUser, data, isLoading, error } = useRegisterUser();
+  const { loginUser, isLoading, error } = useLoginUser();
 
-  const onFormSubmit = async (formData: RegisterSchema) => {
-    await registerUser({...formData});
+  const onFormSubmit = async (formData: LoginSchema) => {
+    await loginUser({ ...formData });
   };
 
   return (
     <form onSubmit={handleSubmit(onFormSubmit)} className="space-y-4">
-      <div>
-        <Label htmlFor="firstname">First name</Label>
-        <CustomInput
-          id="firstname"
-          placeholder="Jesse"
-          register={register("first_name")}
-        />
-        {errors.first_name && <p className="text-red-600">{errors.first_name.message}</p>}
-      </div>
-
-      <div>
-        <Label htmlFor="lastname">Last name</Label>
-        <CustomInput
-          id="lastname"
-          placeholder="Pinkman"
-          register={register("last_name")}
-        />
-        {errors.last_name && <p className="text-red-600">{errors.last_name.message}</p>}
-      </div>
-
       <div>
         <Label htmlFor="username">Username</Label>
         <CustomInput
@@ -47,29 +31,10 @@ const RegisterForm: React.FC = () => {
           placeholder="Jessepinkman123"
           register={register("username")}
         />
-        {errors.username && <p className="text-red-600">{errors.username.message}</p>}
+        {errors.username && (
+          <p className="text-red-600">{errors.username.message}</p>
+        )}
       </div>
-
-      <div>
-        <Label htmlFor="email">Email</Label>
-        <CustomInput
-          id="email"
-          placeholder="jesse.pinkman@example.com"
-          register={register("email")}
-        />
-        {errors.email && <p className="text-red-600">{errors.email.message}</p>}
-      </div>
-
-      <div>
-        <Label htmlFor="hospital_name">Hospital name</Label>
-        <CustomInput
-          id="hospital_name"
-          placeholder="Hospital name"
-          register={register("hospital_name")}
-        />
-        {errors.hospital_name && <p className="text-red-600">{errors.hospital_name?.message}</p>}
-      </div>
-
       <div>
         <Label htmlFor="password">Password</Label>
         <CustomInput
@@ -78,20 +43,10 @@ const RegisterForm: React.FC = () => {
           placeholder="Enter your password"
           register={register("password")}
         />
-        {errors.password && <p className="text-red-600">{errors.password.message}</p>}
+        {errors.password && (
+          <p className="text-red-600">{errors.password.message}</p>
+        )}
       </div>
-
-      <div>
-        <Label htmlFor="confirmPassword">Confirm Password</Label>
-        <CustomInput
-          id="confirmPassword"
-          type={showPassword ? "text" : "password"}
-          placeholder="Confirm your password"
-          register={register("confirmPassword")}
-        />
-        {errors.confirmPassword && <p className="text-red-600">{errors.confirmPassword.message}</p>}
-      </div>
-
       <div className="flex items-center">
         <input
           id="showPassword"
@@ -107,17 +62,14 @@ const RegisterForm: React.FC = () => {
           Show Password
         </Label>
       </div>
-
       <button
         type="submit"
         className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         disabled={isLoading}
       >
-        {isLoading ? "Registering..." : "Register"}
+        {isLoading ? "Logging in..." : "Login"}
       </button>
-
-      {error && <p className="text-red-600 mt-2">{error}</p>}
-      {data && <p className="text-green-600 mt-2">Registration successful!</p>}
+      {error && <p className="text-red-600 mt-2">{error}</p>}{" "}
     </form>
   );
 };
--- src/pages/login/validations/loginSchema.ts
+++ src/pages/login/validations/loginSchema.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const loginSchema = z.object({
     username: z.string().min(1, { message: "Username is required" }),
-    password: z.string().min(6, { message: "Password must be at least 6 characters" }),
+    password: z.string().nonempty(),
 });
 
 export type LoginSchema = z.infer<typeof loginSchema>;
