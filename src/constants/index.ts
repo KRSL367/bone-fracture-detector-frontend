8794a07cf38a2755c2326cb956f1431a41aa1e4d
KRSL367 <krishalpudasaini12345@gmail.com>
2024-08-03 20:48:51 +0545

    Adding the constants folder with navigation

---
+++ src/constants/index.ts
@@ -0,0 +1,34 @@
+
+export const navigation = [
+    {
+      id: "0",
+      title: "Home",
+      url: "/",
+    },
+    {
+      id: "1",
+      title: "Features",
+      url: "/#features",
+    },
+    {
+      id: "2",
+      title: "How to Use",
+      url: "/#how-to-use",
+    },
+    {
+      id: "3",
+      title: "Roadmap",
+      url: "/#roadmap",
+    },
+    {
+      id: "4",
+      title: "Register",
+      url: "/register",
+    },
+    {
+      id: "5",
+      title: "Login",
+      url: "/login",
+    },
+  ];
+  
