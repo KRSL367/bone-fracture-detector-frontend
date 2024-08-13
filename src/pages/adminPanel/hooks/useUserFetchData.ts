90943368e9e5c751e064c950313106c5149c45df
KRSL367 <krishalpudasaini12345@gmail.com>
2024-08-13 13:48:17 +0545

    Create the hook to fetch userdata

---
+++ src/pages/adminPanel/hooks/useUserFetchData.ts
@@ -0,0 +1,22 @@
+import { ApiResponse, useFetchData } from "../../../hooks/useFetchData";
+
+export interface User {
+    id: string;
+    first_name: string;
+    last_name: string;
+    username: string;
+    email: string;
+  }
+
+const useUserFetchData = async (): Promise<ApiResponse<User>> => {
+  const url = `/auth/users/`;
+  const response = await useFetchData<User>(url);
+
+  return {
+    data: response.data,
+    status: response.status,
+    error: response.error,
+  };
+};
+
+export { useUserFetchData };
